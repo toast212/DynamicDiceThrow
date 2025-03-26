2c41d0547c9942420c3ffc2ee7fb261d3e989b8a
toast212 <156926783+toast212@users.noreply.github.com>
2025-03-26 15:25:15 -0400

    Implement orientation-specific fragment management for DieThrow app

--- app/src/main/java/edu/temple/dicethrow/MainActivity.kt
+++ app/src/main/java/edu/temple/dicethrow/MainActivity.kt
@@ -1,22 +1,8 @@
 package edu.temple.dicethrow
 
+import android.content.res.Configuration
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-
-
-/*
-Our DieThrow application has been refactored to move the dieRoll() logic
-into the ViewModel instead of the Fragment.
-Study the DieViewModel, ButtonFragment, and DieFragment classes to
-see the changes.
-
-Follow the requirements below to have this app function
-in both portrait and landscape configurations.
-The Activity layout files for both Portrait and Landscape are already provided
-*/
 
 class MainActivity : AppCompatActivity(), ButtonFragment.ButtonInterface {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -27,16 +13,35 @@ class MainActivity : AppCompatActivity(), ButtonFragment.ButtonInterface {
             - Show _only_ ButtonFragment if portrait
             - show _both_ fragments if Landscape
           */
+        if (savedInstanceState == null) {
+            val isLandscape = resources.configuration.orientation == Configuration.ORIENTATION_LANDSCAPE
+
+            // Always load the button fragment in container1
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container1, ButtonFragment())
+                .commit()
+
+            // Load the die fragment only if in landscape mode in container2
+            if (isLandscape) {
+                supportFragmentManager.beginTransaction()
+                    .replace(R.id.container2, DieFragment())
+                    .commit()
+            }
+        }
     }
 
-    /* TODO 2: switch fragments if die rolled and in portrait (no need to switch fragments if Landscape)
-        */
+    /* TODO 2: switch fragments if die rolled and in portrait (no need to switch fragments if Landscape) */
 
     // This callback function gets invoked when the child Fragment invokes it
     // Remember to place Fragment transactions on BackStack so then can be reversed
     override fun buttonClicked() {
-
+        // Check if we're in portrait mode
+        if (resources.configuration.orientation == Configuration.ORIENTATION_PORTRAIT) {
+            // Replace the button fragment with the die fragment
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container1, DieFragment())
+                .addToBackStack(null)
+                .commit()
+        }
     }
-
-
 }
